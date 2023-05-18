684e58af14b6dcf7c3c0a1e4609be377cfba9659
Argia <argiaunsen372@gmail.com>
2023-05-18 20:52:09 -0300

    creo index js

---
+++ index.js
@@ -0,0 +1,25 @@
+// ejercicio clase 10
+// primera parte
+
+const nombre = "Argia";
+const apellido = "Unsen";
+
+
+console.log(nombre + " " + apellido)
+
+//segunda parte
+
+const edad = 32
+const cantidadDeHijos = 1
+
+let suma = edad + cantidadDeHijos
+console.log(suma)
+
+const años = suma + 7
+console.log(años)
+
+
+
+let curiosidad = nombre + edad
+console.log(curiosidad)
+
