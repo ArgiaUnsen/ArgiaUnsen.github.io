203c9d07a822d160a22d052fa11f0553afa59edb
Argia <argiaunsen372@gmail.com>
2023-05-24 20:41:32 -0300

    cambio js

--- index.js
+++ index.js
@@ -23,3 +23,42 @@ console.log(años)
 let curiosidad = nombre + edad
 console.log(curiosidad)
 
+//switch case
+const palabra = "hola";
+switch (palabra) {
+  case "hola":
+    console.log("buenos dias");
+    break;
+  case "adios":
+    console.log("hasta pronto");
+    break;
+  default:
+    console.log("Hola mundo");
+    break;
+}
+
+//for
+const frutas = ["🍎", "🍏"];
+for (let index = 0; index < frutas.length; index++) {
+  console.log("El elemento es: ", frutas[index]);
+}
+
+//metodos de array: push
+const gente = ["MARCOS", "PRISCILA", "RAMIRO"];
+gente.push("NATALIA");
+console.log(gente);
+
+//metodos de array : pop
+const nombres = ["MARCOS", "PRISCILA", "RAMIRO"];
+nombres.push("NATALIA");
+console.log(nombres);
+nombres.pop();
+console.log(nombres);
+
+//while
+let count = 50;
+let contador = 0;
+while (count > contador) {
+  console.log("Hola chicos RD");
+  contador++;
+}
