f268230a15b4a8862c4abde3a6158addc89c2c23
Argia <argiaunsen372@gmail.com>
2023-05-29 19:54:22 -0300

    cambio js

--- index.js
+++ index.js
@@ -93,15 +93,15 @@
 // }
 
 
-// //Ejercitacion clase 12
+//Ejercitacion clase 12
 
-// let persona = window.prompt("ingrese su nombre")
-// function saludo(nombre) {
-//   if (nombre === ""){
-//     alert("Ingrese nombre")
-//   }
-//   else {alert("Bienvenido " + nombre)}
-// }
-// saludo (persona);
+let persona = window.prompt("ingrese su nombre")
+function saludo(nombre) {
+  if (nombre === ""){
+    alert("Ingrese nombre")
+  }
+  else {alert("Bienvenido " + nombre)}
+}
+saludo (persona);
 
 
